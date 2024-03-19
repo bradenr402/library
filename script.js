cb523470617e94e877fecdb63dea1466244a19f7
bradenr402 <bradenr402@gmail.com>
2024-03-19 10:54:09 -0400

    remove default values from Book constructor

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const library = [];
 
 class Book {
-  constructor(title = 'Unknown', author = 'Unknown', pages = 0, read = false) {
+  constructor(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
